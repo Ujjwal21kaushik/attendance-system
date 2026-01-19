be021566e30b55ea5b695a40224dff25967d45a0
Ujjwal Kaushik <ujjwalkaus21@gmail.com>
2026-01-20 01:52:00 +0530

    do it

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@(0%r)tvc)ulziv^tfcl0dybw9_n-^r2b$qw#%9#vdg)d*rpz@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1","*"]
 
 # Application definition
 
